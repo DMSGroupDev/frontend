9cac35922ba8594d1c9402fac2fc44d7be1e511a
Petra DIVIŠOVÁ <petra.divisova@icnet.eu>
2021-10-12 09:35:05 +0200

    preparing for manage Domain

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import * as React from "react";
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource} from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import Dashboard from './pages/Dashboard';
 import authProvider from './helpers/authProvider.js';
 import LoginPage from './pages/InitUser.js';
-import { UserList } from './components/admin/Users';
+import { DomainList, DomainEdit, DomainCreate } from './components/admin/Domain';
+import { UserList, UserEdit, UserCreate } from './components/admin/User';
+import UserIcon from '@material-ui/icons/Group';
+import DomainIcon from '@material-ui/icons/Domain';
 import MyLayout from './components/common/MyLayout.js'
-import CssBaseline from '@mui/material/CssBaseline';
 import './css/MuiCustom.css';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-    <React.Fragment>
-        <CssBaseline />
         <Admin
             loginPage={LoginPage}
             dashboard={Dashboard}
@@ -20,9 +20,9 @@ const App = () => (
             authProvider={authProvider}
             dataProvider={dataProvider}
         >
-            <Resource name="users" list={UserList} />
+        <Resource name="domains" list={DomainList} edit={DomainEdit} create={DomainCreate} icon={DomainIcon} />
+        <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} icon={UserIcon} />
         </Admin>
-    </React.Fragment>
 )
 
 export default App;
